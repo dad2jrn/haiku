1c30bf8de6e86d00fede696ef9edb4cea1c6d931
dad2jrn <dad2jrn@gmail.com>
2023-09-07 11:22:12 -0400

    copy to clipboard button

--- static/script.js
+++ static/script.js
@@ -1,11 +1,29 @@
-function fetchHaiku() {
-    fetch('/haiku')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('haiku').textContent = data.haiku;
-        })
-        .catch(err => {
-            console.error(err);
-            alert('Error fetching haiku.');
-        });
+async function fetchHaiku() {
+    try {
+        const response = await fetch('/haiku');
+        const data = await response.json();
+        const haikuDiv = document.getElementById('haiku');
+        haikuDiv.textContent = data.haiku;
+
+        // Display the copy-to-clipboard glyph
+        const copyGlyph = document.getElementById('copyGlyph');
+        copyGlyph.style.display = 'inline-block';
+
+    } catch (error) {
+        console.error('Error fetching haiku:', error);
+    }
+}
+
+function copyToClipboard() {
+    const haikuText = document.getElementById('haiku').textContent;
+
+    // Create a temporary textarea to copy the content
+    const textarea = document.createElement('textarea');
+    textarea.value = haikuText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+
+    alert('Haiku copied to clipboard!');
 }
