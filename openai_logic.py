d48cff6838232ca458ed0fd154a0585cd1065d5b
dad2jrn <dad2jrn@gmail.com>
2023-09-07 10:05:59 -0400

    call openai method

--- openai_logic.py
+++ openai_logic.py
@@ -27,10 +27,36 @@ def handle_openai_errors(func) -> Optional[str]:
     return wrapper
 
 class ChatGPT:
-    """Generates a chatbot using the OpenAI GPT-3.5 Turbo model.
+    """
+    Generate a haiku using GPT-3.5 Turbo
     """
     def __init__(self, api_key: str) -> None:
+        """
+        Initialize with the API key
+
+        Args:
+            api_key (str): OpenAI API Key for GPT-3.5 Turbo
+        """
         self.api_key = api_key
         openai.api_key = self.api_key
 
-    @
+    @handle_openai_errors
+    def generate(self, messages) -> Optional[str]:
+        """Generates a chatbot response.
+
+        Args:
+            messages (List[str]): The messages to be sent to the chatbot.
+
+        Returns:
+            Optional[str]: The string representation of an OpenAIError, if one occurs,
+                        or None if the function succeeds without raising an exception.
+        """
+        # Make the API call to generate the haiku
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+
+        # Extract and return the haiku from the API response
+        haiku = response['choices'][0]['message']['content']
+        return haiku.strip()
